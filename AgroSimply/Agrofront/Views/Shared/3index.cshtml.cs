20828892335702c5bc8a1f4d6482cdfb8fa56b93
Pedro Luiz <pedroluiz9922@gmail.com>
2023-06-14 13:55:40 -0300

    Inclusão do front

---
+++ AgroSimply/Agrofront/Views/Shared/3index.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Agrofront.Views.Shared
+{
+    public class _3indexModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
